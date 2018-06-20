0855277d2a2c83d326e8d4a23ff81323d1f40310
Luis Robles <roblescoulter@gmail.com>
2018-06-20 18:54:36 +0800

    MOving word embeddings

---
+++ word_embeddings.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Word embeddings helper for text classification
+"""
+
+from glove import Corpus, Glove
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from gensim.models import word2vec
+from gensim.models import KeyedVectors
+from keras.layers import Embedding
+
+__version__ = '0.0.1'
+
+def create_glove_embeddings(tokenizer, EMBEDDING_DIM, MAX_NUM_WORDS, data = None, use_text8 = False, LEARNING_RATE=0.05, EPOCHS=30, NO_THREADS=4):
+    model = None
+    if(use_text8):
+        model = Glove.load('models/glovetext8.model')
+    else:
+        if(data != None):
+            corpus = Corpus()
+            corpus.fit(data, window=10)
+            model = Glove(no_components=EMBEDDING_DIM,learning_rate=LEARNING_RATE)
+            model.fit(corpus.matrix, epochs=EPOCHS,no_threads=NO_THREADS,verbose=True)
+            model.add_dictionary(corpus.dictionary)
+        else:
+            print('No data found. Using text8 Corpus')
+            model = Glove.load('models/glovetext8.model')
+    
+    embeddings_index = {}
+    for word,index in model.dictionary.items():
+        embeddings_index[word] = model.word_vectors[index]
+    
+    embedding_matrix = np.zeros((MAX_NUM_WORDS, EMBEDDING_DIM))
+    
+    for word, i in tokenizer.word_index.items():
+        if i >= MAX_NUM_WORDS:
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if(embedding_vector is not None):
+            embedding_matrix[i] = embedding_vector
+    return Embedding(input_dim=MAX_NUM_WORDS, output_dim=EMBEDDING_DIM,
+                     weights=[embedding_matrix], trainable=True)
+
+def create_word2vec_embeddings(tokenizer, EMBEDDING_DIM, MAX_NUM_WORDS, data = None, use_text8 = False):
+    model = None
+    if(use_text8):
+        model = KeyedVectors.load_word2vec_format('models/text8.model.bin',binary=True)
+    else:
+        if(data != None):  
+            model = word2vec.Word2Vec(data, size=EMBEDDING_DIM)
+        else:
+            print('No data found. Using text8 Corpus')
+            model = KeyedVectors.load_word2vec_format('models/text8.model.bin',binary=True)
+    
+    embeddings_index = {}
+    for word in model.wv.index2word:
+        embeddings_index[word] = model[word]
+        
+    embedding_matrix = np.zeros((MAX_NUM_WORDS, EMBEDDING_DIM))
+    
+    for word, i in tokenizer.word_index.items():
+        if i >= MAX_NUM_WORDS:
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if(embedding_vector is not None):
+            embedding_matrix[i] = embedding_vector
+    return Embedding(input_dim=MAX_NUM_WORDS, output_dim=EMBEDDING_DIM, input_length = MAX_SEQ_LENGTH,
+                    weights= [embedding_matrix], trainable=True)
+
+def generate_word2vec_text8(EMBEDDING_DIM, saveTo = 'models/text8.model'):
+    sentences = word2vec.Text8Corpus('data/text8')
+    model = word2vec.Word2Vec(sentences, size=EMBEDDING_DIM)
+    model.save(saveTo)
+    model.wv.save_word2vec_format(saveTo + '.bin', binary=True)
+    print('DONE! Saved to', saveTo)
+
+def generate_glove_text8(EMBEDDING_DIM, saveTo = 'models/glovetext8.model'):
+    import itertools
+    sentences = list(itertools.islice(word2vec.Text8Corpus('data/text8'),None))
+    corpus = Corpus()
+    corpus.fit(sentences, window=10)
+    glove = Glove(no_components=EMBEDDING_DIM,learning_rate=0.05)
+    glove.fit(corpus.matrix, epochs=30,no_threads=4,verbose=True)
+    glove.add_dictionary(corpus.dictionary)
+    glove.save(saveTo)
+    print('DONE! Saved to', saveTo)
